8f3950bd662130bf1f04e11da6dbe8dcd5c8c344
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-09-06 15:02:26 +0200

    starting project task_board_app

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+/** @format */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+	return <div>Cardboard App</div>;
+}
+
+export default App;
