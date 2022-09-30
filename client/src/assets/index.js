c4f61fcdd0605aea3ae5ac86780f3d6063aff2bc
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-09-30 13:17:27 +0200

    adding imgs and assets index

---
+++ client/src/assets/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const assets = {
+	images: {
+		logoDark: require('./images/TaskBoard_dark.png'),
+		logoLight: require('./images/TaskBoard_Light.png'),
+		appIcon: require('./images/TaskBoard_Icon.png'),
+	},
+	colors: {
+		secondary: '#292929',
+	},
+};
