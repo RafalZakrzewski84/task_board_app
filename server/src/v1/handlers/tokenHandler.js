2bf2dc74bdb5411b067e131a3ff994806189bb2a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-03 13:20:52 +0200

    adding token verification

---
+++ server/src/v1/handlers/tokenHandler.js
@@ -0,0 +1,38 @@
+/** @format */
+
+const jsonwebtoken = require('jsonwebtoken');
+const User = require('../models/users');
+
+const tokenDecode = (req) => {
+	console.log('tokenHandler', req.headers);
+	const bearerHeader = req.headers['authorization'];
+	if (bearerHeader) {
+		const bearer = bearerHeader.split(' ')[1];
+		try {
+			const tokenDecoded = jsonwebtoken.verify(
+				bearer,
+				process.env.TOKEN_SECRET_KEY
+			);
+
+			return tokenDecoded;
+		} catch {
+			return false;
+		}
+	} else {
+		return false;
+	}
+};
+
+exports.verifyToken = async (req, res, next) {
+    const tokenDecoded = tokenDecode(req)
+    console.log('verifyToken', tokenDecoded)
+    
+    if(tokenDecoded) {
+        const user = await User.findById(tokenDecoded.id)
+        if(!user) return res.status(401).json('Unauthorized')
+        req.user = user
+        next()
+    } else {
+        if(!user) return res.status(401).json('Unauthorized')
+    }
+}
