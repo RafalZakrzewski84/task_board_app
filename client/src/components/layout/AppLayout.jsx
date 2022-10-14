5ea88558e3290b4a41234df09b12d2d4828ecaeb
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-14 14:18:34 +0200

    adding app layout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,13 +1,44 @@
 /** @format */
 
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import authUtils from '../../utils/authUtils';
+import Loading from '../common/Loading';
+import Sidebar from '../common/Sidebar';
 
 const AppLayout = () => {
-	return (
-		<div>
-			<Outlet />
-		</div>
+	const navigate = useNavigate();
+	//set to false to artificially turn off loader
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			const user = await authUtils.isAuthenticated();
+			if (!user) {
+				navigate('/login');
+			} else {
+				setLoading(false);
+			}
+			checkAuth();
+		};
+	}, [navigate]);
+
+	return loading ? (
+		<Loading fullHeight />
+	) : (
+		<Box sx={{ display: 'flex' }}>
+			<Sidebar />
+			<Box
+				sx={{
+					flexGrow: 1,
+					p: 1,
+					width: 'max-content',
+				}}>
+				<Outlet />
+			</Box>
+		</Box>
 	);
 };
 
