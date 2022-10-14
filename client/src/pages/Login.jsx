6edd6592adc1d915feaf6c182fd7125daad0ea75
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-14 14:59:11 +0200

    structuring loading page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,47 @@
 /** @format */
 
-import React from 'react';
+import { useState } from 'react';
+import { Box, TextField } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const Login = () => {
-	return <div>Login</div>;
+	const [loading, setLoading] = useState(false);
+	const handleSubmit = () => {};
+
+	return (
+		<>
+			<Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="username"
+					label="Username"
+					name="username"
+					disabled={loading}
+				/>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="password"
+					label="password"
+					name="password"
+					type="password"
+					disabled={loading}
+				/>
+				<LoadingButton
+					sx={{ mt: 3, mb: 2 }}
+					variant="outlined"
+					fullWidth
+					color="success"
+					type="submit"
+					loading={loading}>
+					Login
+				</LoadingButton>
+			</Box>
+		</>
+	);
 };
 
 export default Login;
