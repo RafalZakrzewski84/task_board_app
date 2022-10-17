5f65e09b542341f376d1922c1390c7110242b5c9
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-17 13:42:57 +0200

    setting user slice and store

---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/userSlice';
+
+export const store = configureStore({
+	reducer: {
+		user: userReducer,
+	},
+});
