c7d660b03a5f50cd53306b9a83d02b25d5a5cb74
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-03 18:44:07 +0100

    adding section and task schema

---
+++ server/src/v1/models/section.js
@@ -0,0 +1,30 @@
+/** @format */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schemaOptions = require('./modelOptions');
+
+const taskSchema = Schema(
+	{
+		section: {
+			type: Schema.Types.ObjectId,
+			ref: 'Section',
+			required: true,
+		},
+		title: {
+			type: String,
+			default: '',
+		},
+		content: {
+			type: String,
+			default: '',
+		},
+		position: {
+			type: Number,
+		},
+	},
+	schemaOptions
+);
+
+module.exports = mongoose.model('Task', taskSchema);
