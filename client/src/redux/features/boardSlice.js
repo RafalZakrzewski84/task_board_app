33730f642aa859b6b27780770021295718c544ba
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-03 19:42:30 +0100

    adding board slice in redux

---
+++ client/src/redux/features/boardSlice.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { value: [] };
+
+export const boardSlice = createSlice({
+	name: 'board',
+	initialState,
+	reducers: {
+		setBoards: (state, action) => {
+			state.value = action.value;
+		},
+	},
+});
+
+export const { setBoards } = boardSlice.actions;
+export default boardSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,11 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/userSlice';
+import boardReducer from './features/boardSlice';
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
+		board: boardReducer,
 	},
 });
