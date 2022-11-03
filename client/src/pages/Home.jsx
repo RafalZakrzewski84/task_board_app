b44e7d2ae9303bc84087d65eaac18e79f3b57186
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-03 17:56:06 +0100

    adding create board button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 /** @format */
-
-import React from 'react';
+import { Box } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const Home = () => {
-	return <div>Home</div>;
+	const createBoard = () => {};
+	return (
+		<Box
+			sx={{
+				height: '100%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
+			<LoadingButton variant="outlined" color="success" onClick={createBoard}>
+				Click here to create your first board
+			</LoadingButton>
+		</Box>
+	);
 };
 
 export default Home;
