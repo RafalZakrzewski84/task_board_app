5e85133a64337ed101b2bf35e7696b8fc165082e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-03 18:16:01 +0100

    adding board schema

---
+++ server/src/v1/models/boards.js
@@ -0,0 +1,42 @@
+/** @format */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schemaOptions = require('./modelOptions');
+
+const boardSchema = Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		icon: {
+			type: String,
+			default: '#',
+		},
+		title: {
+			type: String,
+			default: 'Untitled',
+		},
+		description: {
+			type: String,
+			default: 'Add description here',
+		},
+		position: {
+			type: Number,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
+		favoritePosition: {
+			type: Number,
+			default: 0,
+		},
+	},
+	schemaOptions
+);
+
+module.exports = mongoose.model('Board', boardSchema);
