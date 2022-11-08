34c99d994e62d6d75dd69fec5a415b0196f91e3d
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-08 15:28:07 +0100

    adding create board functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 /** @format */
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import { Box } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 
+import { setBoards } from '../redux/features/boardSlice';
+import boardsApi from '../api/boardsApi';
+
 const Home = () => {
-	const createBoard = () => {};
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(false);
+
+	const createBoard = async () => {
+		try {
+			setLoading(true);
+			const res = await boardsApi.createBoard();
+			dispatch(setBoards([res]));
+			navigate(`/boards/${res._id}`);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<Box
 			sx={{
@@ -12,7 +35,11 @@ const Home = () => {
 				alignItems: 'center',
 				justifyContent: 'center',
 			}}>
-			<LoadingButton variant="outlined" color="success" onClick={createBoard}>
+			<LoadingButton
+				variant="outlined"
+				color="success"
+				onClick={createBoard}
+				loading={loading}>
 				Click here to create your first board
 			</LoadingButton>
 		</Box>
