9863b5c8cbd836a0dbd0d8bb45ed9e1a3120302a
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-09 11:32:52 +0100

    adding update boards position API

--- client/src/api/boardsApi.js
+++ client/src/api/boardsApi.js
@@ -5,6 +5,10 @@ import axiosClient from './axiosClient';
 const boardsApi = {
 	createBoard: () => axiosClient.post('boards'),
 	getAllBoards: () => axiosClient.get('boards'),
+	updateBoardPosition: (params) => {
+		console.log('updateBoardPosition used in boardsApi');
+		axiosClient.put('boards', params);
+	},
 };
 
 export default boardsApi;
